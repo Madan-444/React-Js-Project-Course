61d9709f3c2ccece25e606a937c7a9bd88d4212a
Madan-444 <mmadan3600@gmail.com>
2021-04-01 18:46:25 +0545

    navbar_project completed

--- navbar_project/src/Navbar.js
+++ navbar_project/src/Navbar.js
@@ -4,7 +4,51 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [showLink,setShowLink] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(()=> {
+    const linkHeight = linksRef.current.getBoundingClientRect().height
+    if(showLink) {
+      linksContainerRef.current.style.height = `${linkHeight}px`
+    } else {
+      linksContainerRef.current.style.height = `0px`
+    }
+  },[showLink])
+
+  return <nav>
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo"/>
+        <button className="nav-toggle">
+          <FaBars onClick={()=> setShowLink(!showLink)} />
+        </button>
+      </div>
+
+          <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link)=> {
+              return (
+               <li>
+                 <a key={link.id} href={link.url}>{link.text}</a>
+               </li>
+              )
+            })}
+          </ul>
+        </div>
+
+      <ul className="social-icons">
+          {social.map((item)=> {
+            return (
+              <li>
+                <a keu={item.id} href={item.url}> {item.icon} </a>
+              </li>
+            )
+          })}
+      </ul>
+    </div>
+  </nav>
 }
 
 export default Navbar
